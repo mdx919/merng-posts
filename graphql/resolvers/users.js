50b2d95597d442972c20a2754e60afac16379a08
Owner <Owner@Owners-iMac-4.local>
2020-12-28 17:21:50 -0500

    aded auth routes

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,78 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const { UserInputError } = require('apollo-server')
+
+const { validateRegisterInput, validateLoginInput } = require('../../util/validators')
+const User = require('../../models/User')
+const { SECRET_KEY } = require('../../config')
+
+function generateToken(user) {
+    return jwt.sign({
+        id: user.id,
+        email: user.email,
+        userName: user.userName
+    }, SECRET_KEY, { expiresIn: '1hr' })
+}
+
+module.exports = {
+     Mutation: {
+         async login(_, { userName, password }){
+             const { errors, valid } = validateLoginInput(userName, password)
+             const user = await User.findOne({ userName })
+            if(!valid){
+                throw new UserInputError('Errors', { errors })
+            }
+             if(!user) {
+                 errors.general = 'User not found'
+                 throw new UserInputError('User not found', { errors })
+             }
+
+             const match = await bcrypt.compare(password, user.password)
+             if(!match){
+                 errors.general = 'Wrong Credentials'
+                 throw new UserInputError('Wrong Credentials', { errors })
+             }
+
+             const token = generateToken(user)
+
+             return {
+                ...user._doc,
+                id: user._id,
+                token
+            }
+         },
+         async register(_, { registerInput: { userName, email, password, confirmPassword} }, context, info) {
+             //TODO: validate user data
+             const { valid, errors } = validateRegisterInput(userName, email, password, confirmPassword)
+             if(!valid) {
+                 throw new UserInputError('Errors', { errors })
+             }
+             //TODO: make sure user doesnt exist already
+             const user = await User.findOne({ userName })
+             if(user) {
+                throw new UserInputError('Username is taken')
+                    errors: {
+                        username: 'This username is staken'
+                    }
+             }
+             //hash password and create an auth token
+             password = await bcrypt.hash(password, 12)
+
+             const newUser = new User({
+                 email,
+                 userName,
+                 password,
+                 createdAt: new Date().toISOString()
+             })
+
+             const res = await newUser.save()
+             const token = generateToken(res)
+
+             return {
+                 ...res._doc,
+                 id: res._id,
+                 token
+             }
+         }
+     }
+}
---
+++ graphql/typeDefs.js
@@ -0,0 +1,31 @@
+ const { gql } = require('apollo-server')
+
+module.exports  = gql`
+type Post{
+    id: ID!
+    body: String!
+    createdAt: String!
+    userName: String!
+}
+type User{
+    id: ID!
+    email: String!
+    token: String!
+    userName: String!
+    createdAt: String!
+}
+input RegisterInput{
+    userName: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+}
+type Query{
+    getPosts: [Post]
+}
+type Mutation{
+    register(registerInput: RegisterInput): User!
+    login(userName: String!, password: String!): User!
+}
+
+`
