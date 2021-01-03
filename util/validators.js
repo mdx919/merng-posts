d06359061eda93c864c04e042eb5e619db40b550
Owner <Owner@Owners-iMac-4.local>
2021-01-03 15:35:56 -0500

    fixed register error message

--- util/validators.js
+++ util/validators.js
@@ -1,46 +1,46 @@
 module.exports.validateRegisterInput = (
-    userName,
-    email,
-    password,
-    confirmPassword
+  userName,
+  email,
+  password,
+  confirmPassword
 ) => {
-    const errors = {}
+  const errors = {};
 
-    if(userName.trim() === ''){
-        errors.userName = 'Username must not be empty'
+  if (userName.trim() === '') {
+    errors.userName = 'Username must not be empty';
+  }
+  if (email.trim() === '') {
+    errors.email = 'Email must not be empty';
+  } else {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!email.match(regEx)) {
+      errors.email = 'Email must be a valid email address';
     }
-    if(email.trim() === ''){
-        errors.userName = 'Email must not be empty'
-    } else {
-        const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        if(!email.match(regEx)) {
-            errors.email = 'Email must be a valid email address'
-        }
-    }
-    if(password === '') {
-        errors.password = 'Password must not be empty'
-    } else if (password !== confirmPassword) {
-        errors.password = 'Passwords must match'
-    }
+  }
+  if (password === '') {
+    errors.password = 'Password must not be empty';
+  } else if (password !== confirmPassword) {
+    errors.password = 'Passwords must match';
+  }
 
-    return {
-        errors,
-        valid: Object.keys(errors).length < 1
-    }
-}
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
 
 module.exports.validateLoginInput = (userName, password) => {
-    const errors = {}
-    if(userName.trim() === ''){
-        errors.userName = 'Username must not be empty'
-    }
+  const errors = {};
+  if (userName.trim() === '') {
+    errors.userName = 'Username must not be empty';
+  }
 
-    if(password === '') {
-        errors.password = 'Password must not be empty'
-    }
+  if (password === '') {
+    errors.password = 'Password must not be empty';
+  }
 
-    return {
-        errors,
-        valid: Object.keys(errors).length < 1
-    }
-}
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
